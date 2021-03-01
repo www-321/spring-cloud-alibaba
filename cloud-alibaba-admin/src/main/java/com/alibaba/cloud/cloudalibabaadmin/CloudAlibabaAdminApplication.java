abfc4187cc4926aada96b2b785e0671934963a20
9MFDUAC0SNXCE99\Administrator <wuqn891@163.com>
2021-03-01 15:45:42 +0800

    spring boot admin

---
+++ cloud-alibaba-admin/src/main/java/com/alibaba/cloud/cloudalibabaadmin/CloudAlibabaAdminApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.cloudalibabaadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class CloudAlibabaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaAdminApplication.class, args);
+    }
+
+}
---
+++ cloud-alibaba-admin/src/test/java/com/alibaba/cloud/cloudalibabaadmin/CloudAlibabaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.cloud.cloudalibabaadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAlibabaAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
